3b8fa9f11f4ca437f71ca7a1042b301d3972ba89
partha1194 <30717449+partha1194@users.noreply.github.com>
2020-12-01 08:14:42 +0530

    Add files via upload
    Activate Auto Incremental Archive in Microsoft 365. This is applicable for Exchange Plan 2 or others having unlimited Archival option.

---
+++ Activate-autoIncrement-Archival-Storage.ps1
@@ -0,0 +1,10 @@
+﻿Set-ExecutionPolicy RemoteSigned 
+$UserCredential=Get-Credential
+$s=New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -credential $UserCredential -Authentication Basic -AllowRedirection
+import-PSSession -session $s
+Connect-MsolService -Credential $UserCredential
+Write-host "Prepared MSOL Login"
+$email=Read-Host "Enter the email id for which Archive Auto Expand is to be activated :: "
+Enable-Mailbox $email -AutoExpandingArchive
+Write-Host " Checking if Auto Expanding is enabled for " $email
+Get-Mailbox $email | FL AutoExpandingArchiveEnabled
